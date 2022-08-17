1c443aa344caa43138d4a184641c95ac6daeee03
david <dsolanoreyes@my.wctc.edu>
2022-08-17 01:22:06 -0500

    finito

---
+++ src/components/Country.jsx
@@ -0,0 +1,61 @@
+
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { Box, CardContent, Divider, Card } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Grid } from '@mui/material';
+import { Paper } from '@mui/material';
+import { Button } from '@mui/material';
+import React, { Component } from 'react';
+import '../App.css';
+
+const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+    }));  
+
+
+class Country extends Component {
+    state = { 
+        name: 'United States',
+        gold: 0
+    } 
+    
+    // updates gold stae property (1 click equals 1 gold)
+    handleIncrement = () =>{
+        console.log(this.state.gold)
+        this.setState({ gold: this.state.gold + 1});
+    }
+
+    render() { 
+        return (
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container margin="3px" justify="center" alignItems="center" style={{ minHeight: '10vh' }} columnSpacing={{ xs: 3, sm: 2, md: 3 }}>
+                    <Grid item xs={4}></Grid>
+                    <Grid item xs={4}>
+                        <Card sx={{ p: 1 }}>
+                            <div className='contentGrid'>
+                                {this.state.name}
+                            </div>
+                            
+                            <Divider sx={{ my: 1 }} />
+
+                            <CardContent sx={{ my: .015 }}>
+                                Gold Medals: {this.state.gold}
+                            </CardContent>
+
+                            <div>
+                                <Button className='funkyBtn' onClick={ this.handleIncrement } variant="outlined"><ArrowUpwardIcon /></Button>
+                            </div>
+                        </Card>
+                        
+                    </Grid>
+                    <Grid item xs={4}></Grid>
+                </Grid>
+            </Box>
+        );
+    }
+}
+
+export default Country;
